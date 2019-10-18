77788d1aef245e1eeb3cb3e989d6e61402f3172f
David Hamilton <dahamilton10@yahoo.com>
2019-10-18 12:25:00 -0400

    I FIXED THE QUESTION LOOP
    before the itterator would increase by a power of 2 every question you answer. so i would go 0 1 2 4 8 16, which messes up which questions show up. I just moved the click events for the choices out of the generatequestion function and it got fixed.

--- test.js
+++ test.js
@@ -31,78 +31,74 @@ $("#start-button").click(function (event) {
 
 function startTimer() {
     var interval = setInterval(function () {
-        timeLeft--   
+        timeLeft--
         $("#timer").text("Time left: " + timeLeft);
-        if (timeLeft == 0){
+        if (timeLeft == 0) {
             clearInterval(interval);
         }
     }, 1000);
 }
 
-function generateQuesion(){
-        $("#question-area").text(questions[i].title);
-        $("#choices-area").html('<p class="lead" id="choices-area"></p>');
-        $("#choices-first").text(questions[i].choices[0]);
-        $("#choices-second").text(questions[i].choices[1]);
-        $("#choices-third").text(questions[i].choices[2]);
-        $("#choices-fourth").text(questions[i].choices[3]);
-
-        $("#choices-first").click(function(event){
-            event.prevetDefault
-            if (questions[i].answer == questions[i].choices[0]){
-                questionCorrect()
-            } else {
-                questionWrong()
-            };
-        });
-
-        $("#choices-second").click(function(event){
-            event.prevetDefault
-            if (questions[i].answer == questions[i].choices[1]){
-                questionCorrect()
-            } else {
-                questionWrong()
-            };
-        });
-
-        $("#choices-third").click(function(event){
-            event.prevetDefault
-            if (questions[i].answer == questions[i].choices[2]){
-                questionCorrect()
-            } else {
-                questionWrong()
-            };
-        });
-
-        $("#choices-fourth").click(function(event){
-            event.prevetDefault
-            if (questions[i].answer == questions[i].choices[3]){
-                questionCorrect()
-            } else {
-                questionWrong()
-            };
-        });
+function generateQuesion() {
+    $("#question-area").text(questions[i].title);
+    $("#choices-area").html('<p class="lead" id="choices-area"></p>');
+    $("#choices-first").text(questions[i].choices[0]);
+    $("#choices-second").text(questions[i].choices[1]);
+    $("#choices-third").text(questions[i].choices[2]);
+    $("#choices-fourth").text(questions[i].choices[3]);
 }
 
-function questionCorrect(){
+$("#choices-first").click(function (event) {
+    event.prevetDefault
+    if (questions[i].answer == questions[i].choices[0]) {
+        questionCorrect()
+    } else {
+        questionWrong()
+    };
+});
+
+$("#choices-second").click(function (event) {
+    event.prevetDefault
+    if (questions[i].answer == questions[i].choices[1]) {
+        questionCorrect()
+    } else {
+        questionWrong()
+    };
+});
+
+$("#choices-third").click(function (event) {
+    event.prevetDefault
+    if (questions[i].answer == questions[i].choices[2]) {
+        questionCorrect()
+    } else {
+        questionWrong()
+    };
+});
+
+$("#choices-fourth").click(function (event) {
+    event.prevetDefault
+    if (questions[i].answer == questions[i].choices[3]) {
+        questionCorrect()
+    } else {
+        questionWrong()
+    };
+});
+
+function questionCorrect() {
     $("#choices-area").html('<p class="lead" id="choices-area">Thats Correct!</p>');
+    i++;
     setTimeout(function () {
-        console.log("pre correct " + i)
-        i = i + 1;
-        console.log("post correct " + i);
-        if (i < questions.length){
+        if (i < questions.length) {
             generateQuesion();
         }
-        return;
     }, 1500);
 };
 
-function questionWrong(){
+function questionWrong() {
     $("#choices-area").html('<p class="lead" id="choices-area">Thats Incorrect!</p>');
+    i++;
     setTimeout(function () {
-        i++;
-        console.log("incorrect " + i);
-        if (i < questions.length){
+        if (i < questions.length) {
             generateQuesion();
         }
     }, 1500);

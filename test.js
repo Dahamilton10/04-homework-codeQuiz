b590bc601e62fc97ef10cf2979d38a1dbda565d8
David Hamilton <dahamilton10@yahoo.com>
2019-10-18 12:16:04 -0400

    created test.js which ive been working on, getting closer to questions cycling properly.
    test.js was created because i wanted to try not using a for loop to cycle questions. i would just comment the for loop out but the document was getting cluttered and I just wanted a fresh area to work.

--- test.js
+++ test.js
@@ -19,7 +19,7 @@ var questions = [
 var choicesDiv = $("#choicesDiv");
 var timerElement = $("#timer");
 var timeLeft = questions.length * 15;
-var currentItteration = 0
+var i = 0
 
 $("#question-area").text("Welcome! If you want to play the test just click the button below to get started!")
 $("#choices-area").html("<button type='button' class='btn btn-primary' id='start-button'>Start Quiz</button>")
@@ -29,76 +29,81 @@ $("#start-button").click(function (event) {
     generateQuesion();
 });
 
-function generateQuesion() {
-    for (var i = 0; i < questions.length; i){
-        currentItteration = i
+function startTimer() {
+    var interval = setInterval(function () {
+        timeLeft--   
+        $("#timer").text("Time left: " + timeLeft);
+        if (timeLeft == 0){
+            clearInterval(interval);
+        }
+    }, 1000);
+}
+
+function generateQuesion(){
         $("#question-area").text(questions[i].title);
         $("#choices-area").html('<p class="lead" id="choices-area"></p>');
         $("#choices-first").text(questions[i].choices[0]);
         $("#choices-second").text(questions[i].choices[1]);
         $("#choices-third").text(questions[i].choices[2]);
         $("#choices-fourth").text(questions[i].choices[3]);
-    
-    $("#choices-first").click(function(event){
-        event.prevetDefault
-        if (questions[currentItteration].answer = $("choices-first").text){
-            questionCorrect()
-        } else {
-            questionWrong()
-        };
-    })
-    $("#choices-second").click(function(event){
-        event.prevetDefault
-        if (questions[currentItteration].answer = $("choices-first").text){
-            questionCorrect()
-        } else {
-            questionWrong()
-        };
-    })
-    $("#choices-third").click(function(event){
-        event.prevetDefault
-        if (questions[currentItteration].answer = $("choices-first").text){
-            questionCorrect()
-        } else {
-            questionWrong()
-        };
-    })
-    $("#choices-fourth").click(function(event){
-        event.prevetDefault
-        if (questions[currentItteration].answer = $("choices-first").text){
-            questionCorrect()
-        } else {
-            questionWrong()
-        };
-    })
-};
+
+        $("#choices-first").click(function(event){
+            event.prevetDefault
+            if (questions[i].answer == questions[i].choices[0]){
+                questionCorrect()
+            } else {
+                questionWrong()
+            };
+        });
+
+        $("#choices-second").click(function(event){
+            event.prevetDefault
+            if (questions[i].answer == questions[i].choices[1]){
+                questionCorrect()
+            } else {
+                questionWrong()
+            };
+        });
+
+        $("#choices-third").click(function(event){
+            event.prevetDefault
+            if (questions[i].answer == questions[i].choices[2]){
+                questionCorrect()
+            } else {
+                questionWrong()
+            };
+        });
+
+        $("#choices-fourth").click(function(event){
+            event.prevetDefault
+            if (questions[i].answer == questions[i].choices[3]){
+                questionCorrect()
+            } else {
+                questionWrong()
+            };
+        });
 }
 
 function questionCorrect(){
     $("#choices-area").html('<p class="lead" id="choices-area">Thats Correct!</p>');
     setTimeout(function () {
-        i++;
-    }, 500);
+        console.log("pre correct " + i)
+        i = i + 1;
+        console.log("post correct " + i);
+        if (i < questions.length){
+            generateQuesion();
+        }
+        return;
+    }, 1500);
 };
 
 function questionWrong(){
     $("#choices-area").html('<p class="lead" id="choices-area">Thats Incorrect!</p>');
     setTimeout(function () {
         i++;
-    }, 500);
+        console.log("incorrect " + i);
+        if (i < questions.length){
+            generateQuesion();
+        }
+    }, 1500);
 };
-
-function startTimer() {
-var interval = setInterval(function () {
-    timeLeft--   
-    $("#timer").text("Time left: " + timeLeft);
-    if (timeLeft == 0){
-        clearInterval(interval);
-    }
-}, 1000);
-}
-
-// for (var x = 0; x < questions.choices.length; x++){
-//     var choiceElement = $("<p>" + questions.choices[x] + "</p>")
-//     choicesDiv.append(choiceElement);
-// }
